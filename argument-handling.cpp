9d8331f8f35d7e461ffa8918dfa1e50ce596e45f
Thomas Schiffer <tom.schiffer@gmail.com>
2026-01-16 15:55:59 +0100

    Improve code formatting

--- argument-handling.cpp
+++ argument-handling.cpp
@@ -2,7 +2,7 @@
  * \file
  * C++ 17 argument handling demo program
  */
- 
+
 #include <iostream>
 #include <string_view>
 #include <vector>
@@ -10,9 +10,9 @@
 
 int main(int argc, char *argv[])
 {
-    std::vector<std::string_view> arguments{ argv, argv + argc };
+    std::vector<std::string_view> arguments{argv, argv + argc};
     std::cout << "argc: " << arguments.size() << std::endl;
-    for (const auto& arg : std::as_const(arguments))
+    for (const auto &arg : std::as_const(arguments))
         std::cout << "  arg: " << arg << std::endl;
 
     return EXIT_SUCCESS;
