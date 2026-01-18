c5d0f4951012bfe4e8e10604b5754c6e8db7a60f
Thomas Schiffer <tom.schiffer@gmail.com>
2026-01-18 21:52:03 +0100

    Handle positional arguments and boolean flags in a basic way

--- argument-handling.cpp
+++ argument-handling.cpp
@@ -1,12 +1,19 @@
 /*!
  * \file
- * C++ 17 argument handling demo program
+ * C++ 17 argument handling demo program.
+ *
+ * Argument handling is a very common problem addressed by different libraries like
+ * [Boost program_options](https://www.boost.org/doc/libs/latest/doc/html/program_options.html).
+ * The following program implements a simplified argument handling featuring:
+ *  - a set of optional, boolean argument flags
+ *  - positional arguments
  */
 
 #include <cstdio>
 #include <cstdlib>
 #include <exception>
 #include <iostream>
+#include <set>
 #include <string_view>
 #include <utility>
 #include <vector>
@@ -14,9 +21,23 @@
 int main(int argc, char *argv[])
 try {
     std::vector<std::string_view> arguments{argv, argv + argc};
-    std::cout << "argc: " << arguments.size() << '\n';
-    for (const auto &arg : std::as_const(arguments))
-        std::cout << "arg: " << arg << '\n';
+    std::cout << "argument count: " << arguments.size() << '\n';
+
+    std::set<std::string_view> arguments_flags;
+    std::vector<std::string_view> positional_arguments;
+
+    for (const auto &arg : std::as_const(arguments)) {
+        if (arg[0] == '-')
+            arguments_flags.insert(arg);
+        else
+            positional_arguments.push_back(arg);
+    }
+
+    for (const auto &arg : std::as_const(positional_arguments))
+        std::cout << "positional argument: " << arg << '\n';
+
+    if (arguments_flags.find("-a") != arguments_flags.end())
+        std::cout << "argument flag: -a " << '\n';
 
     return EXIT_SUCCESS;
 }
