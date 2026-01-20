c93c431ce6c45f8edc2921d4ab4a9751d4ccf661
Thomas Schiffer <tom.schiffer@gmail.com>
2026-01-20 09:54:25 +0100

    Move minimalistic testing infrastructure to separate header

--- argument-handling.cpp
+++ argument-handling.cpp
@@ -9,6 +9,8 @@
  *  - positional arguments
  */
 
+#include "test.h"
+
 #include <cstdio>
 #include <cstdlib>
 #include <exception>
@@ -40,43 +42,34 @@ parseArguments(const std::vector<std::string_view> &arguments)
     return {positional_arguments, options};
 }
 
-int runTests()
+
+void testsParseArguments()
 {
-    int return_value = EXIT_SUCCESS;
-    // NOLINTNEXTLINE(readability-identifier-naming)
-    auto checkThat = [&return_value](auto expr, int line = __builtin_LINE(), const char *file = __builtin_FILE()) {
-        if (!expr) {
-            return_value = EXIT_FAILURE;
-            std::fprintf(stderr, "%s:%i: check failed!\n", file, line);
-        }
-    };
     {
         auto [positionals, options] = parseArguments({"myexe", "1", "2"});
-        checkThat(options.empty());
-        checkThat(positionals.size() == 3);
+        test(options.empty());
+        test(positionals.size() == 3);
     }
     {
         auto [positionals, options] = parseArguments({"myexe", "-a", "2"});
-        checkThat(options.find("-a") != options.end());
-        checkThat(positionals.size() == 2);
+        test(options.find("-a") != options.end());
+        test(positionals.size() == 2);
     }
     {
         auto [positionals, options] = parseArguments({"myexe", "--", "-a", "2"});
-        checkThat(options.empty());
-        checkThat(positionals.size() == 3);
-        checkThat(positionals[1] == "-a");
+        test(options.empty());
+        test(positionals.size() == 3);
+        test(positionals[1] == "-a");
     }
-    return return_value;
 }
 
 } // namespace
 
 int main(int argc, char *argv[])
 try {
-    const std::vector<std::string_view> arguments{argv, argv + argc};
+    test.runIfRequested(argc, argv, testsParseArguments);
 
-    if (arguments.size() >= 2 && arguments[1] == "-test")
-        return runTests();
+    const std::vector<std::string_view> arguments{argv, argv + argc};
 
     auto [positional_arguments, options] = parseArguments(arguments);
 
---
+++ test.h
@@ -0,0 +1,33 @@
+/*!
+ * \file
+ * Minimalistic test framework.
+ */
+
+#pragma once
+
+#include <cstdlib>
+#include <string_view>
+
+struct Test {
+    Test() = default;
+    ~Test() = default;
+
+    void operator()(bool expr, int line = __builtin_LINE(), const char *file = __builtin_FILE())
+    {
+        if (!expr) {
+            result = EXIT_FAILURE;
+            std::fprintf(stderr, "%s:%i: check failed!\n", file, line);
+        }
+    }
+    // NOLINTNEXTLINE(modernize-avoid-c-arrays)
+    template <typename F> void runIfRequested(int argc, char *argv[], F run_tests)
+    {
+        using namespace std::string_view_literals;
+        if (argc >= 2 && argv[1] == "-test"sv) {
+            run_tests();
+            std::exit(result);
+        }
+    }
+
+    int result = EXIT_SUCCESS;
+} test;
